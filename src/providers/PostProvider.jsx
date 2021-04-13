948f1981f3e4d65270d299ec49c8818a1e81509b
Leno <akrachunov@gmail.com>
2021-04-13 21:27:30 +0100

    Add Post Provider - context API.

---
+++ src/providers/PostProvider.jsx
@@ -0,0 +1,33 @@
+import React, { Component, createContext } from 'react';
+import { firestore } from '../firebase';
+import { collectIdsAndData } from '../utilities';
+
+export const PostsContext = createContext();
+
+class PostsProvider extends Component {
+    state = { posts: [] };
+
+    unsubscribe = null;
+
+    componentDidMount = () => {
+        this.unsubscribe = firestore.collection('posts').onSnapshot(snapshot => {
+            const posts = snapshot.docs.map(collectIdsAndData);
+            this.setState({ posts });
+        });
+    };
+
+    componentWillUnmount = () => {
+        this.unsubscribe();
+    };
+
+    render() {
+        const { posts } = this.state;
+        const { children } = this.props;
+
+        return (
+            <PostsContext.Provider value={posts}>{children}</PostsContext.Provider>
+        );
+    }
+}
+
+export default PostsProvider;
